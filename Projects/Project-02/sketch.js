c4f66ed4f336068ccf24e0e6a13e83274f0c6a88
Kermen <kermenchoung@Kermens-MacBook-Pro.local>
2017-04-23 16:50:11 -0700

    Guy

--- Projects/Project-02/sketch.js
+++ Projects/Project-02/sketch.js
@@ -1,12 +1,12 @@
 var chaos = true;
 var order = false;
-var orderGrid = [];
+var orderGrid;
 var orderImagesRevealed = 0;
 var imgs = [];
 
 function preload(){
-    imgs[0] = loadImage("158.jpg");
-    imgs[1] = loadImage("wrong_image.jpg");
+    imgs[0] = loadImage("https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fmedia-cdn.tripadvisor.com%2Fmedia%2Fphoto-s%2F01%2Fe6%2Fa3%2F10%2Fhotel-hallway.jpg&f=1");
+    imgs[1] = loadImage("https://images.duckduckgo.com/iu/?u=http%3A%2F%2Ffarm2.staticflickr.com%2F1385%2F1262833993_b4b2ec0ba9_z.jpg&f=1");
 }
 
 function setup(){
@@ -40,6 +40,7 @@ function chaosDraw(){
 }
 
 function orderSetup(){
+    orderGrid = [];
     for (var y = 75; y < height + 150; y += 150){
         for (var x = 150; x < width + 300; x += 300){
             orderGrid.push({x: x, y: y, img: false});
@@ -63,6 +64,14 @@ function orderDraw(){
 }
 
 function mouseClicked(){
+    handleClick();
+}
+
+function touchStarted(){
+    handleClick();
+}
+
+function handleClick(){
     if (chaos){
         chaos = false;
         order = true;
