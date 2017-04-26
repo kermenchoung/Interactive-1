35c1b5ca1165f29e3a91083ce8a0b047b114ae6e
Kermen <kermenchoung@Kermens-MacBook-Pro.local>
2017-04-25 17:50:32 -0700

    ;iugyu

---
+++ Exercises/Exercise-11/site/accounts/calikermedia.php
@@ -0,0 +1,8 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: calikermedia
+password: >
+  $2a$10$FwYb6INZjHPGLz.IK6aQHOSbC0X9EYOviU99RUkro.nlzISS.S1vO
+email: kermenchoung@cca.edu
+language: en
+role: admin
